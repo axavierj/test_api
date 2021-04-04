2b7f94dbd8880e6c270488aaa8811c48c174c237
axavierj <alexanderjimenez29@gmail.com>
2021-04-04 03:32:13 -0400

    initial setup done

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/meal_prep_app", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("db up");
+});
 
 app.listen(3000, () => {
   console.log("server up");
